e90b033c920895b7425ff8ee5c2a7f325f4762d5
Jeremy Rand <jeremyrand@airmail.cc>
2021-02-07 16:11:01 +0000

    Fix case typo

--- x509/install.sh
+++ x509/install.sh
@@ -5,7 +5,7 @@ shopt -s failglob
 
 cp -a $(go env GOROOT)/src/crypto/x509/* ./
 
-sed -i "s/Elliptic.Unmarshal(/Elliptic.UnmarshalCompressed(/g" ./*.go
-sed -i "s/Elliptic.Marshal(/Elliptic.MarshalCompressed(/g" ./*.go
+sed -i "s/elliptic.Unmarshal(/elliptic.UnmarshalCompressed(/g" ./*.go
+sed -i "s/elliptic.Marshal(/elliptic.MarshalCompressed(/g" ./*.go
 
 rm ./*_test.go
