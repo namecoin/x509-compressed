744fc368aedc54f99bb529e7cceb0c9695a57caa
Jeremy Rand <jeremyrand@airmail.cc>
2021-02-07 17:06:52 +0000

    Fix macOS build error due to internal package import

--- x509/install.sh
+++ x509/install.sh
@@ -7,5 +7,6 @@ cp -a $(go env GOROOT)/src/crypto/x509/* ./
 
 sed -i "s/elliptic.Unmarshal(/elliptic.UnmarshalCompressed(/g" ./*.go
 sed -i "s/elliptic.Marshal(/elliptic.MarshalCompressed(/g" ./*.go
+sed -i 's|"crypto/x509/internal|"github.com/namecoin/x509-compressed/x509/internal|g' ./*.go
 
 rm ./*_test.go
