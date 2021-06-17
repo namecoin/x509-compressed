179cd0b7efeb3295caf24197c73876f46c17f90f
Jeremy Rand <jeremyrand@airmail.cc>
2021-06-17 05:12:42 +0000

    Don't error if tests are already removed

--- x509/install.sh
+++ x509/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -eu -o pipefail
-shopt -s failglob
+shopt -s nullglob
 
 cp -a $(go env GOROOT)/src/crypto/x509/* ./
 
@@ -11,4 +11,10 @@ sed -i "s/elliptic.Marshal(/elliptic.MarshalCompressed(/g" ./*.go
 # These files use an internal package, we provide a skeleton to make it compile
 rm -f ./root_darwin_amd64.go ./root_darwin.go
 
-rm ./*_test.go
+# Delete tests if they're present.  (On some distros, e.g. Fedora, they're
+# already removed.)
+for f in ./*_test.go
+do
+  rm ./*_test.go
+  break
+done
